b91cc9ac522e4b3680037ddeabb3ca0ae06658d4
Fernando Frias <fernandofrias@gmail.com>
2025-03-23 14:01:03 -0300

    06-12-GifGrid - Nuevo componente

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 
 export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
-  const onAddCategory = ( newCategory ) => {
+  const onAddCategory = (newCategory) => {
 
-    if(categories.includes(newCategory)) {
-      console.log("ya existe");  
-      return;}
+    if (categories.includes(newCategory)) {
+      console.log("ya existe");
+      return;
+    }
     console.log(newCategory);
 
     setCategories([newCategory, ...categories]);
@@ -18,25 +20,20 @@ export const GifExpertApp = () => {
 
   return (
     <>
-      {/* titulo */}
       <h1>GifExpertApp</h1>
 
-      {/* input */}
-      <AddCategory 
-        //setearCategoria={setCategories}   
-        onNewCategory = { (value) => onAddCategory(value) }         
+      <AddCategory
+        onNewCategory={(value) => onAddCategory(value)}
       />
 
-      {/* listado de gifs */}
-      <ol>
-          {categories.map( categoria => {
-            return (<li key={categoria}>{categoria}</li>)
-          })}
-      </ol>
-
-
-
-      {/* gif item */}
+      {categories.map(categoria => {
+        return (
+          <GifGrid 
+            key={categoria}
+            catego={categoria}
+          />
+        )
+      })}
 
     </>
   )
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,10 @@
+
+
+export const GifGrid = ({ catego }) => {
+  return (
+    <>
+      <h3>{ catego }</h3>
+      <p>Hola mundo</p>
+    </>
+  )
+}
